9ab326adfd7c30af7ced23b0da60a1a4dd927ea4
Tim Clemans <tim@insideyourgovernment.com>
2016-04-04 05:25:18 +0000

    allow user to get list of tables for user

--- index.js
+++ index.js
@@ -18,22 +18,24 @@ server.get('/:user/:table', function(req, res, next) {
 	if (!req.query) {
         req.query = '';
     }
-	
-	// Parse query into AST
-	var ast = parser.parse(req.query);
-	
-	// Process SELECT columns
-	//ast.columns = processSelect(ast.columns, tables[req.params.table]);
-	
-	// Add FROM table to AST
-	ast.from = [{table: '"' + req.params.table + '"'}];
-	
-	// Process WHERE recursively
-	if(ast.where) ast.where = processWhere(ast.where);
-	
-	// Convert AST back to SQL
-	var sql = parser.stringify.parse(ast);
-	
+	if (req.params.table == 'tables') {
+        var sql = "SELECT CDB_UserTables('all') as table";
+    } else {
+	    // Parse query into AST
+        var ast = parser.parse(req.query);
+
+        // Process SELECT columns
+        //ast.columns = processSelect(ast.columns, tables[req.params.table]);
+
+        // Add FROM table to AST
+        ast.from = [{table: '"' + req.params.table + '"'}];
+
+        // Process WHERE recursively
+        if(ast.where) ast.where = processWhere(ast.where);
+
+        // Convert AST back to SQL
+        var sql = parser.stringify.parse(ast);
+    }
     var url = 'https://'+req.params.user+'.cartodb.com/api/v2/sql?q='+encodeURIComponent(sql);
     console.log(url);
     console.log(sql);
