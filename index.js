d768ae65a3e60de9529bfeabb4812fe502f27cf5
Tim Clemans <tim@insideyourgovernment.com>
2016-04-04 04:21:40 +0000

    trying to fix bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var parser = require('node-tims-custom-soql-parser'),
+var parser = require('node-soda2-parser'),
 	restify = require('restify'),
 	_ = require('underscore'),
 	anyDB = require('any-db-postgres'),
