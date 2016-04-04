5f39bf5eff6098907f442f926a081809b8b8a234
Tim Clemans <tim@insideyourgovernment.com>
2016-04-04 06:16:12 +0000

    fix port issue on shared machine

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ server.get('/:user/:table', function(req, res, next) {
 });
 
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';	
 server.listen(server_port, server_host, function() {
     console.log('%s listening at %s', server.name, server.url);
